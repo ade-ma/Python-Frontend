db4e2f3ebb5235aaa1b3ae1d4e7845e85c1fc7ee
Madison May <madison.may@students.olin.edu>
2014-02-11 00:15:19 -0500

    Cleaning up

--- client.py
+++ client.py
@@ -1,22 +1,30 @@
-import socket
+import socket, time
 import capnp
-import flask
-from config import socket_file, schema_file
+from flask import Flask
+from config import socket_file, schema_file, poll_interval
 
-schemas = capnp.load(schema_file)
+app = Flask(__name__)
 
-# create socket object
-s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-s.connect(socket_file)
+def listen(s):
+    while True:
+        try:
+            # receive binary data
+            binary_data = s.recv(4096)
+            if binary_data:
+                # parse as Reading object 
+                print schemas.Reading.from_bytes(binary_data)
+            time.sleep(poll_interval)
 
-while True:
-    try:
-        # receive binary data
-        binary_data = s.recv(4096)
-        if binary_data:
-            # parse as Reading object 
-            print schemas.Reading.from_bytes(binary_data)
+        except KeyboardInterrupt:
+            # close connection
+            s.close()
 
-    except KeyboardInterrupt:
-        # close connection
-        s.close()
+if __name__ == "__main__":
+    schemas = capnp.load(schema_file)
+
+    # create socket object
+    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    s.connect(socket_file)
+
+    # begin listening for data emitted
+    listen(s)
